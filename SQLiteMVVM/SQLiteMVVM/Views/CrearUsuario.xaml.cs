d0196cd15a09939ec54584e71d0b6b7922531d02
natbai1106 <natbai1106@gmail.com>
2021-08-18 20:30:25 -0600

    Se creo el MVVM

---
+++ SQLiteMVVM/SQLiteMVVM/Views/CrearUsuario.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Plugin.Media;
+using SQLiteMVVM.Models;
+using SQLiteMVVM.Utils;
+using SQLiteMVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SQLiteMVVM.Views
+{
+    public partial class CrearUsuario : ContentPage
+    {
+        public CrearUsuario()
+        {
+            InitializeComponent();
+            //BindingContext = new UsersViewModel(this);
+        }
+    }
+}
---
+++ SQLiteMVVM/SQLiteMVVM/Views/Reservacion.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SQLiteMVVM.ViewModels;
+using SQLiteMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SQLiteMVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Reservacion : ContentPage
+    {
+        public Reservacion()
+        {
+            InitializeComponent();
+            //BindingContext = new ReservationViewModel(this);
+        }       
+
+        private void ListServices_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var viewModel = BindingContext as ServicesViewModels;
+            var service = e.Item as Service;
+        }
+    }
+}
